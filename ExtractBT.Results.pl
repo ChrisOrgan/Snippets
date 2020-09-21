dec16ad20cd6d3635501c133937d372e78028189
ChrisOrgan <organchris@gmail.com>
2020-09-21 10:00:41 -0600

    edit

---
+++ ExtractBT.Results.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+#This script extracts results from a merged BayesTraits Output
+#By Chris Organ, Dec 14th, 2010
+#
+
+	( $results_source ) = @ARGV;
+	$usage = "perl Extract.Results.pl source_file";
+	unless ( $results_source )
+	{ die "\nUsage: $usage\n" };
+	open( SOURCE, "$results_source" ) or die "Could not open file";
+	open (DESTINATION,">>ExtractedResults.txt") or die;
+	$line_count = 0;
+		while ($line = <SOURCE>) {
+		   $line_count++;
+		if($line =~ m/^1\t/)
+		  {print DESTINATION  "$line"} 
+		}
+	close (DESTINATION);
+
+
+		
+	
+	
+	
